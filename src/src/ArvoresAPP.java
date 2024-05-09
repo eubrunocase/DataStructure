5fd412f3ba31020a876b339464339748759146b8
Bruno Casé <bruno.case45@gmail.com>
2024-05-09 14:07:22 -0300

    arvores 0.1

---
+++ src/src/ArvoresAPP.java
@@ -0,0 +1,15 @@
+public class ArvoresAPP {
+    public static void main(String[] args) {
+        BinarySearchTree MinhaABB = new BinarySearchTree();
+
+        MinhaABB.insert(6);
+        MinhaABB.insert(8);
+        MinhaABB.insert(5);
+        MinhaABB.insert(7);
+        MinhaABB.insert(15);
+
+        MinhaABB.find(5);
+
+
+    }
+}
---
+++ src/src/BinarySearchTree.java
@@ -0,0 +1,78 @@
+public class BinarySearchTree {
+    private BinaryNode root;
+
+    /*---------- NÓ DE UMA ÁRVORE BINÁRIA ----------*/
+    class BinaryNode {
+        private int element;
+        private BinaryNode left;
+        private BinaryNode right;
+
+        /*---------- Construtor do nó----------*/
+        public BinaryNode(int e, BinaryNode l, BinaryNode r) {
+            element = e;
+            left = l;
+            right = r;
+        }
+    }
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    /*---------- Esvazia a árvore ----------*/
+    public void clear() {
+        root = null;
+    }
+
+    /*---------- Verifica se está vazia ----------*/
+    public boolean isEmpty() {
+        return (root == null);
+    }
+
+    /*---------- Metodos de inserção abaixo ----------*/
+    public void insert(int x) {
+        root = insert(x, root);
+    }
+
+    private BinaryNode insert(int x, BinaryNode t) {
+        if (t == null) {
+            t = new BinaryNode(x, null, null);
+        } else if (x < t.element) {
+            t.left = insert(x, t.left);
+        } else if (x > t.element) {
+            t.right = insert(x, t.right);
+        }
+        return t;
+    }
+
+
+    public void print() {
+        if (isEmpty())
+            System.out.println("Arvore vazia");
+        else
+            print(root);
+    }
+
+    private void print(BinaryNode t) {
+        if (t != null) {
+            System.out.println(t.element);
+            print(t.left);
+            print(t.right);
+
+        }
+    }
+
+
+    public void find(int x) {
+        if (root == null) {
+            System.out.println("Arvore vazia");
+        } else {
+            for (BinaryNode t = root; t != null; t = t.left) {
+                if (t.element == x) {
+                    System.out.println(t.element);
+                }
+            }
+        }
+    }
+
+}
